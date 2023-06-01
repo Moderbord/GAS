df01985cda9c8959ee2f521499350549febe2a5b
Pontus Andersson <pontus_andersson1@live.se>
2023-06-01 14:51:54 +0200

    Moved CharacterAttributeSetBase sources files due to typo
    << Source\GAS\Private\Character\Abilites
    >> Source\GAS\Private\Character\Abilities

--- Source/GAS/Private/Character/Abilities/AttributeSets/CharacterAttributeSetBase.cpp
+++ Source/GAS/Private/Character/Abilities/AttributeSets/CharacterAttributeSetBase.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Character/Abilites/AttributeSets/CharacterAttributeSetBase.h"
+#include "Character/Abilities/AttributeSets/CharacterAttributeSetBase.h"
 #include "Net/UnrealNetwork.h"
 
 void UCharacterAttributeSetBase::OnRep_Level(const FGameplayAttributeData& OldLevel)
