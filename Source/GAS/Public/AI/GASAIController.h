90cfc4ca091196a413e36b2e4ef3fac55ff6eb97
Pontus Andersson <pontus_andersson1@live.se>
2023-06-05 15:10:52 +0200

    Add basic AIController (GASAIController)

---
+++ Source/GAS/Public/AI/GASAIController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "GASAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API AGASAIController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	AGASAIController();
+};
