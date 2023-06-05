2996641f4f4500ae1cbb698152ca340ecd886aad
Pontus Andersson <pontus_andersson1@live.se>
2023-06-05 14:38:46 +0200

    Added InputAbilityID enumeration for ability activation states

--- Source/GAS/GAS.h
+++ Source/GAS/GAS.h
@@ -3,3 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
+UENUM(BlueprintType)
+enum class GASAbilityID : uint8
+{
+	None UMETA(DisplayName = "None"),
+	Confirm UMETA(DisplayName = "Confirm"),
+	Cancel UMETA(DisplayName = "Cancel")
+};
