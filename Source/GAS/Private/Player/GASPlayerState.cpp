c1998f595a56eefa24c54f3047f9358fe11d1a50
Pontus Andersson <pontus_andersson1@live.se>
2023-06-07 10:12:07 +0200

    Add basic implementation of GASPlayerState

---
+++ Source/GAS/Private/Player/GASPlayerState.cpp
@@ -0,0 +1,117 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/GASPlayerState.h"
+
+AGASPlayerState::AGASPlayerState()
+{
+	AbilitySystemComponent = CreateDefaultSubobject<UCharacterAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+
+	AttributeSetBase = CreateDefaultSubobject<UCharacterAttributeSetBase>(TEXT("AttributeSetBase)"));
+
+	NetUpdateFrequency = 100.f;
+
+	DeadTag = FGameplayTag::RequestGameplayTag(FName("State.Dead"));
+}
+
+UCharacterAbilitySystemComponent* AGASPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+UCharacterAttributeSetBase* AGASPlayerState::GetAttributeSetBase() const
+{
+	return AttributeSetBase;
+}
+
+bool AGASPlayerState::IsAlive() const
+{
+	return GetHealth() > 0.f;
+}
+
+void AGASPlayerState::ShowAbilityConfirmCancelText(bool ShowText)
+{
+	//TODO UI implementation
+}
+
+float AGASPlayerState::GetCharacterLevel() const
+{
+	return AttributeSetBase->GetLevel();
+}
+
+float AGASPlayerState::GetHealth() const
+{
+	return AttributeSetBase->GetHealth();
+}
+
+float AGASPlayerState::GetMaxHealth() const
+{
+	return AttributeSetBase->GetMaxHealth();
+}
+
+float AGASPlayerState::GetMana() const
+{
+	return AttributeSetBase->GetMana();
+}
+
+float AGASPlayerState::GetMaxMana() const
+{
+	return AttributeSetBase->GetMaxMana();
+}
+
+void AGASPlayerState::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (AbilitySystemComponent)
+	{
+		CharacterLevelChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetLevelAttribute()).AddUObject(this, &AGASPlayerState::CharacterLevelChanged);
+		HealthChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetHealthAttribute()).AddUObject(this, &AGASPlayerState::HealthChanged);
+		MaxHealthChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetMaxHealthAttribute()).AddUObject(this, &AGASPlayerState::MaxHealthChanged);
+		ManaChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetManaAttribute()).AddUObject(this, &AGASPlayerState::ManaChanged);
+		MaxManaChangedDelegateHandle = AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AttributeSetBase->GetMaxManaAttribute()).AddUObject(this, &AGASPlayerState::MaxManaChanged);
+
+		AbilitySystemComponent->RegisterGameplayTagEvent(FGameplayTag::RequestGameplayTag(FName("State.Debuff.Stun")), EGameplayTagEventType::NewOrRemoved).AddUObject(this, &AGASPlayerState::StunTagChanged);
+	}
+}
+
+void AGASPlayerState::CharacterLevelChanged(const FOnAttributeChangeData& Data)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Character Level Changed"));
+}
+
+void AGASPlayerState::HealthChanged(const FOnAttributeChangeData& Data)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Health Changed"));
+}
+
+void AGASPlayerState::MaxHealthChanged(const FOnAttributeChangeData& Data)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Max Health Changed"));
+}
+
+void AGASPlayerState::ManaChanged(const FOnAttributeChangeData& Data)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Maxa Changed"));
+}
+
+void AGASPlayerState::MaxManaChanged(const FOnAttributeChangeData& Data)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Max Mana Changed"));
+}
+
+void AGASPlayerState::StunTagChanged(const FGameplayTag CallbackTag, int32 NewCount)
+{
+	if (NewCount > 0)
+	{
+		FGameplayTagContainer AbilityTagsToCancel;
+		AbilityTagsToCancel.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability")));
+	
+		FGameplayTagContainer AbilityTagsToIgnore;
+		AbilityTagsToIgnore.AddTag(FGameplayTag::RequestGameplayTag(FName("Ability.NotCanceledByStun")));
+
+		AbilitySystemComponent->CancelAbilities(&AbilityTagsToCancel, &AbilityTagsToIgnore);
+	}
+}
---
+++ Source/GAS/Public/Player/GASPlayerState.h
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "AbilitySystemInterface.h"
+#include "Character/Abilities/CharacterAbilitySystemComponent.h"
+#include "Character/Abilities/AttributeSets/CharacterAttributeSetBase.h"
+#include "GASPlayerState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API AGASPlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AGASPlayerState();
+
+	class UCharacterAbilitySystemComponent* GetAbilitySystemComponent() const override;
+
+	class UCharacterAttributeSetBase* GetAttributeSetBase() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Demo|GASPlayerState")
+	bool IsAlive() const;
+	
+	UFUNCTION(BlueprintCallable, Category = "Demo|GASPlayerState")
+	void ShowAbilityConfirmCancelText(bool ShowText);
+
+	UFUNCTION(BlueprintCallable, Category = "Demo|GASPlayerState|Attributes")
+	float GetCharacterLevel() const;
+	
+	UFUNCTION(BlueprintCallable, Category = "Demo|GASPlayerState|Attributes")
+	float GetHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Demo|GASPlayerState|Attributes")
+	float GetMaxHealth() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Demo|GASPlayerState|Attributes")
+	float GetMana() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Demo|GASPlayerState|Attributes")
+	float GetMaxMana() const;
+
+protected:
+
+	UPROPERTY()
+	class UCharacterAbilitySystemComponent* AbilitySystemComponent;
+	
+	UPROPERTY()
+	class UCharacterAttributeSetBase* AttributeSetBase;
+
+	FGameplayTag DeadTag;
+
+	FDelegateHandle CharacterLevelChangedDelegateHandle;
+	FDelegateHandle HealthChangedDelegateHandle;
+	FDelegateHandle MaxHealthChangedDelegateHandle;
+	FDelegateHandle ManaChangedDelegateHandle;
+	FDelegateHandle MaxManaChangedDelegateHandle;
+
+	virtual void BeginPlay() override;
+
+	virtual void CharacterLevelChanged(const FOnAttributeChangeData& Data);
+	virtual void HealthChanged(const FOnAttributeChangeData& Data);
+	virtual void MaxHealthChanged(const FOnAttributeChangeData& Data);
+	virtual void ManaChanged(const FOnAttributeChangeData& Data);
+	virtual void MaxManaChanged(const FOnAttributeChangeData& Data);
+
+	virtual void StunTagChanged(const FGameplayTag CallbackTag, int32 NewCount);
+
+
+};
