93bcb320d7d1e91dbc80c5bdd47c0a0fbf17d2b4
Pontus Andersson <pontus_andersson1@live.se>
2023-06-07 21:13:37 +0200

    Add the new hero character as the default player pawn class

--- Source/GAS/Private/GASGameMode.cpp
+++ Source/GAS/Private/GASGameMode.cpp
@@ -1,15 +1,22 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
 #include "GASGameMode.h"
-#include "Character/GASCharacterBase.h"
+#include "Character/Hero/GASHeroCharacter.h"
+#include "Player/GASPlayerController.h"
+#include "Player/GASPlayerState.h"
 #include "UObject/ConstructorHelpers.h"
 
 AGASGameMode::AGASGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/Characters/Hero/BP_GASHeroCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
+
+	//static ConstructorHelpers::FClassFinder<APlayerState> PlayerStateBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_GASHeroCharacter"));
+
+	//static ConstructorHelpers::FClassFinder<APlayerController> PlayerControllerBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_GASHeroCharacter"));
+
 }
