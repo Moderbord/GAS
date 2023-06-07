f7041ed3f21cc070f7bd08bbe4886c408180b272
Pontus Andersson <pontus_andersson1@live.se>
2023-06-07 20:53:38 +0200

    Add a base character class and derived hero character class, as well as reorganize #include statements
    Previous character implementation contained camera & input functionality unnecessary for any non-player derived class.

--- Source/GAS/Public/Character/GASCharacterBase.h
+++ Source/GAS/Public/Character/GASCharacterBase.h
@@ -4,13 +4,9 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "InputActionValue.h"
-#include "GameplayTagContainer.h"
 #include "AbilitySystemInterface.h"
+#include "GameplayTagContainer.h"
 #include "GAS/GAS.h"
-#include "Abilities/CharacterAbilitySystemComponent.h"
-#include "Abilities/AttributeSets/CharacterAttributeSetBase.h"
-#include "Abilities/CharacterGameplayAbility.h"
 #include "GASCharacterBase.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FCharacterDiedDelegate, AGASCharacterBase*, character);
@@ -45,7 +41,6 @@ public:
 	/**
 	* Getters for attributes from GDAttributeSetBase
 	**/
-
 	UFUNCTION(BlueprintCallable, Category = "GAS|Character|Attributes")
 	int32 GetCharacterLevel() const;
 	
@@ -64,10 +59,6 @@ public:
 	// Implemented IAbilitySystemInterface
 	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
-	// Getters for subobjects
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
-
 protected:
 
 	TWeakObjectPtr<class UCharacterAbilitySystemComponent> AbilitySystemComponent;
@@ -82,21 +73,18 @@ protected:
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Demo|Animation")
 	UAnimMontage* DeathMontage;
 
-	//UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Abilities")
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Abilities")
 	TArray<TSubclassOf<class UCharacterGameplayAbility>> CharacterAbilities;
 
-	//UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Abilities")
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Abilities")
 	TSubclassOf<class UGameplayEffect> DefaultAttributes;
 
-	//UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Abilities")
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "GAS|Abilities")
 	TArray<TSubclassOf<class UGameplayEffect>> StartupEffects;
 
-	// APawn interface
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
 	// To add mapping context
-	virtual void BeginPlay();
-	
+	virtual void BeginPlay() override;
+
 	virtual void AddCharacterAbilites();
 
 	virtual void InitializeAttributes();
@@ -106,37 +94,5 @@ protected:
 	virtual void SetHealth(float Health);
 
 	virtual void SetMana(float Mana);
-
-	/** Called for movement input */
-	void Move(const FInputActionValue& Value);
-
-	/** Called for looking input */
-	void Look(const FInputActionValue& Value);
-
-private:
-	/** Camera boom positioning the camera behind the character */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class USpringArmComponent* CameraBoom;
-
-	/** Follow camera */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
-	class UCameraComponent* FollowCamera;
-	
-	/** MappingContext */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	class UInputMappingContext* DefaultMappingContext;
-
-	/** Jump Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	class UInputAction* JumpAction;
-
-	/** Move Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	class UInputAction* MoveAction;
-
-	/** Look Input Action */
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
-	class UInputAction* LookAction;
-
 };
 
---
+++ Source/GAS/Public/Character/Hero/GASHeroCharacter.h
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "InputActionValue.h"
+#include "Character/GASCharacterBase.h"
+#include "GASHeroCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAS_API AGASHeroCharacter : public AGASCharacterBase
+{
+	GENERATED_BODY()
+
+public:
+	AGASHeroCharacter(const class FObjectInitializer& ObjectInitializer);
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	virtual void PossessedBy(AController* NewController) override;
+
+	// Getters for subobjects
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+
+	UFUNCTION(BlueprintCallable, Category = "GAS|Camera")
+	float GetStartingCameraBoomArmLength();
+
+	UFUNCTION(BlueprintCallable, Category = "GAS|Camera")
+	FVector GetStartingCameraBoomLocation();
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	virtual void OnRep_PlayerState() override;
+
+	void InitializeStartingValues(class AGASPlayerState* PS);
+
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+private:
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* MoveAction;
+
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* LookAction;
+};
